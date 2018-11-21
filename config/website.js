b89e14faf5d2db11b97f24ae29ef9bb7ffa4be39
Artur Maslov <aartasas@gmail.com>
2018-11-21 20:14:57 +0200

    Modify gatsby template with personal info

---
+++ config/website.js
@@ -0,0 +1,21 @@
+const tailwind = require('../tailwind');
+
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
+
+  siteTitle: 'Artur Maslov Portfolio', // Navigation and Site Title
+  siteTitleAlt: 'Cara', // Alternative Site title for SEO
+  siteUrl: 'http://arturmaslov.com', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logos/logo_512.png', // Used for SEO and manifest
+  siteDescription: 'Portfolio of web & app developer',
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@neonartur', // Twitter Username
+  ogSiteName: 'artur.maslov', // Facebook Site Name
+  ogLanguage: 'en_US', // Facebook Language
+
+  // Manifest and Progress color
+  themeColor: tailwind.colors.orange,
+  backgroundColor: tailwind.colors.blue,
+};
